4a09c255f7a9ee94eb2b4c351d18154f6f2a501b
Sadat40 <sahmed40@stuy.edu>
2023-08-03 11:50:27 -0400

    done

--- intervals/script.js
+++ intervals/script.js
@@ -1,5 +1,13 @@
 // number of seconds that have passed
 let time = 0;
+time++;
 // reference to the <span> containing the number
 let counterElement = document.getElementById("time");
+
+intervalId = setInterval(updateCountdown, 1000);
+
+function updateCountdown() {
+    counterElement.innerText=time;
+    time++;
+}
 
--- race-to-the-finish/sketch.js
+++ race-to-the-finish/sketch.js
@@ -16,37 +16,37 @@ function draw() {
     background(0, 200, 0);
 
     // move both balls random distances
-    redXPos += random(-1, 5);
+    redXPos += random(0, 2);
     redYPos += random(-0.5, 0.5);
-    blueXPos += random(-1, 5);
+    blueXPos += random(0, 2);
     blueYPos += random(-0.5, 0.5);
 
     // show scores
     textSize(60);
     fill(255, 0, 0);
-    text(redScore, 800, 100);
+    text(redScore, 650, 100);
     fill(0, 0, 255);
-    text(blueScore, 800, 225);
+    text(blueScore, 650, 225);
 
     // show finish line
-    line(600, 0 600, 300);
+    line(600, 0, 600, 300);
 
     // red ball
     fill(255, 0, 0);
-    ellipse(50, 50, redXPos, redYPos);
+    ellipse(redXPos, redYPos,50,50);
     
     // blue ball
     fill(0, 0, 255);
-    ellipse(50, 50, blueXPos, blueYPos);
+    ellipse(blueXPos, blueYPos,50,50);
 
     // crossed finish line
-    if (redXPos < 575 || blueXPos < 575) {
+    if (redXPos > 575 || blueXPos >575) {
         if (redXPos > blueXPos) {
             // blue wins
             redScore += 1;
         } else if (blueXPos > redXPos) {
             // red wins
-            blueScore = 1;
+            blueScore += 1;
         }
         // reset positions
         redXPos = 25;
